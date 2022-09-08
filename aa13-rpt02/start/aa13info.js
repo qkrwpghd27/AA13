6bdfe130b690c6ca6ffd65b1de648dd988a063d9
HM11, AR11 <100752556+qkrwpghd27@users.noreply.github.com>
2022-09-08 16:26:16 +0900

    Add files via upload

---
+++ aa13-rpt02/start/aa13info.js
@@ -0,0 +1,5 @@
+var myinfo = require('./index_aa13');
+
+myinfo("aa12", "qkrwpghd0327","010-2353-6923");
+
+myinfo("aa55", "Comsi","010-1234-5678");
---
+++ aa13-rpt02/start/hello_function.js
@@ -0,0 +1,6 @@
+function hello(what){
+    console.log("Hello" + what + " !");
+}
+
+hello("aa13");
+hello("ParkJEHong, 박제홍");
